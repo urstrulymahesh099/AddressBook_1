778e0209165604cc7f6d004fd05df0828e50e310
mago mahi <maheshhonnamurthy099@gmail.com>
2023-01-10 17:24:04 +0530

    [Mahesh]Add:EditContact

--- AddressBook_1/AddressBook.cs
+++ AddressBook_1/AddressBook.cs
@@ -1,5 +1,4 @@
-﻿using AddressBook;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,35 +6,94 @@ using System.Threading.Tasks;
 
 namespace AddressBook_1
 {
-
-    public class Addressbook
+    public class AddressBook
     {
         public string FirstName, LastName, Address, City, State, Email;
         public int Zip;
-        public long PhoneNumber;
-        public Addressbook[] ContactArray;
+        public long PhoneNum;
+        public AddressBook[] ContactArray;
         public int Contact = 0;
-        public Addressbook()
+        public AddressBook()
         {
-            this.ContactArray = new Addressbook[5];
+            this.ContactArray = new AddressBook[5];
         }
-        public Addressbook(string firstName, string lastName, string address, string city, string state, string Email, int zip, long phoneNumber)
+        public AddressBook(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNum)
         {
             FirstName = firstName;
             LastName = lastName;
             Address = address;
             City = city;
             State = state;
-            Email = Email;
+            Email = email;
             Zip = zip;
-            PhoneNumber = phoneNumber;
+            PhoneNum = phoneNum;
         }
-        public void CreatContact(string firstName, string lastName, string address, string city, string state, string Email, int zip, long phoneNumber)
+        public void CreateContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNum)
         {
-            ContactArray[this.Contact] = new Addressbook(firstName, lastName, address, city, state, Email, zip, phoneNumber);
+            ContactArray[this.Contact] = new AddressBook(firstName, lastName, address, city, state, email, zip, phoneNum);
             Contact++;
-            program pr = new program();
-            pr.DisplayContact(ContactArray, Contact);
+            Program program = new Program();
+            program.DisplayContacts(ContactArray, Contact);
+
+        }
+        public void EditContact()
+        {
+            int i = 0;
+            Console.WriteLine("Enter First Name to Edit");
+            string FirstName = Console.ReadLine();
+            while (ContactArray[i].FirstName != FirstName)
+            {
+                i++;
+            }
+            Console.WriteLine("Enter Field ToBE Modify\n1.FirstNmae\n2.LastName\n3.Address\n4.City\n5.State\n6.Zip\n7.Email\n8.PhoneNUmber");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    string FName = Console.ReadLine();
+                    ContactArray[i].FirstName = FName;
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    string LName = Console.ReadLine();
+                    ContactArray[i].LastName = LName;
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    string Add = Console.ReadLine();
+                    ContactArray[i].Address = Add;
+                    break;
+                case 4:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    string city = Console.ReadLine();
+                    ContactArray[i].City = city;
+                    break;
+                case 5:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    string StateN = Console.ReadLine();
+                    ContactArray[i].State = StateN;
+                    break;
+                case 6:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    int ZipN = Convert.ToInt32(Console.ReadLine());
+                    ContactArray[i].Zip = ZipN;
+                    break;
+                case 7:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    string MailID = Console.ReadLine();
+                    ContactArray[i].Email = MailID;
+                    break;
+                case 8:
+                    Console.WriteLine("Enter the MOdifed Value");
+                    long PhnNum = Convert.ToInt64(Console.ReadLine());
+                    ContactArray[i].PhoneNum = PhnNum;
+                    break;
+
+
+            }
+            Program pr = new Program();
+            pr.DisplayContacts(ContactArray, Contact);
         }
 
     }
--- AddressBook_1/Program.cs
+++ AddressBook_1/Program.cs
@@ -1,19 +1,18 @@
-﻿using AddressBook_1;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBook
+namespace AddressBook_1
 {
-    public class program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Address Book program");
-            Addressbook add = new Addressbook();
-            Console.WriteLine("Enter Number of contact to add");
+            Console.WriteLine("Welcome To Address BOOK Program");
+            AddressBook add = new AddressBook();
+            Console.WriteLine("Enter Number Of Contact To Add");
             int Contact = Convert.ToInt32(Console.ReadLine());
             while (Contact > 0)
             {
@@ -27,29 +26,34 @@ namespace AddressBook
                 string City = Console.ReadLine();
                 Console.WriteLine("Enter State");
                 string State = Console.ReadLine();
-                Console.WriteLine("Enter Email");
-                string Email = Console.ReadLine();
                 Console.WriteLine("Enter Zip");
                 int Zip = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Eamil");
+                string Email = Console.ReadLine();
                 Console.WriteLine("Enter PhoneNumber");
-                long PhoneNumber = Convert.ToInt64(Console.ReadLine());
-                add.CreatContact(FirstName, LastName, Address, City, State, Email, Zip, PhoneNumber);
+                long PhoneNum = Convert.ToInt64(Console.ReadLine());
+                add.CreateContact(FirstName, LastName, Address, City, State, Email, Zip, PhoneNum);
                 Contact--;
             }
+            Console.WriteLine("Do YOu Want To MOdify(Y/N)");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'Y' || ch == 'y')
+            {
+                add.EditContact();
+            }
         }
-        public void DisplayContact(Addressbook[] ContactArray, int n)
+        public void DisplayContacts(AddressBook[] ContactArray, int n)
         {
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Name {0} {1},", ContactArray[i].FirstName, ContactArray[i].LastName);
-                Console.WriteLine("Adrdess {0} ", ContactArray[i].Address);
-                Console.WriteLine("City {0}", ContactArray[i].City);
-                Console.WriteLine("State {0} ", ContactArray[i].State);
-                Console.WriteLine("Email {0}", ContactArray[i].Email);
-                Console.WriteLine("Zip {0}", ContactArray[i].Zip);
-                Console.WriteLine("PhoneNumber {0}", ContactArray[i].PhoneNumber);
+                Console.WriteLine("Name {0} {1}", ContactArray[i].FirstName, ContactArray[i].LastName);
+                Console.WriteLine("Address : {0}", ContactArray[i].Address);
+                Console.WriteLine("City :{0}", ContactArray[i].City);
+                Console.WriteLine("State : {0}", ContactArray[i].State);
+                Console.WriteLine("Email :{0}", ContactArray[i].Email);
+                Console.WriteLine("Zip :{0}", ContactArray[i].Zip);
+                Console.WriteLine("Phone :{0}", ContactArray[i].PhoneNum);
             }
         }
-
     }
 }
