04dc7ce86026ef570dca7df05e5d60dbcd71de60
mago mahi <maheshhonnamurthy099@gmail.com>
2023-01-10 16:54:56 +0530

    [Mahesh]Add:CreateContact

---
+++ AddressBook_1/AddressBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_1
+{
+
+    internal class Addressbook
+    {
+        public string FirstName, LastName, Address, City, State, email;
+        public int Zip;
+        public long PhoneNumber;
+        public Addressbook(string firstName, string lastName, string address, string city, string state, string Email, int zip, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Email = email;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+        }
+        public void DisplayContect()
+        {
+            Console.WriteLine("Name:   {0} {1},", FirstName, LastName);
+            Console.WriteLine("Adrdess:  {0} ", Address);
+            Console.WriteLine("City:  {0}", City);
+            Console.WriteLine("State: {0} ", State);
+            Console.WriteLine("Email: {0}", email);
+            Console.WriteLine("Zip: {0}", Zip);
+            Console.WriteLine("PhoneNumber: {0}", PhoneNumber);
+        }
+    }
+}
--- AddressBook_1/Program.cs
+++ AddressBook_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AddressBook_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Address Book program");
+            Addressbook Add = new Addressbook("Mahesh", "H", "KGF", "kolar", "Karnataka", "mahesh@gmail.com", 56434, 7829303710);
+            Add.DisplayContect();
+
+        }
+    }
+}
